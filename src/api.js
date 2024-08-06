18165c4807e1099f501bbd973624fe9999eb1703
sreevidya-alluri <sreevidya.alluri@gmail.com>
2024-08-06 19:16:08 +0530

    msg

--- src/api.js
+++ src/api.js
@@ -4,26 +4,26 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const API_BASE_URL='https://apex-plus-backend-xxx.vercel.app';
 export const getPokemons = () => {
-  return axios.get(${API_BASE_URL}/api/pokemons);
+  return axios.get(`${API_BASE_URL}/api/pokemons`);
 };
 
 export const addPokemon = (pokemon) => {
-  return axios.post(${API_BASE_URL}/api/pokemons, pokemon);
+  return axios.post(`${API_BASE_URL}/api/pokemons, pokemon`);
 };
 
 export const updatePokemon = (id, pokemon) => {
-  return axios.put(${API_BASE_URL}/api/pokemons/${id}, pokemon);
+  return axios.put(`${API_BASE_URL}/api/pokemons/${id}, pokemon`);
 };
 
 export const deletePokemon = (id) => {
-  return axios.delete(${API_BASE_URL}/api/pokemons/${id});
+  return axios.delete(`${API_BASE_URL}/api/pokemons/${id}`);
 };
 
 export const getPokemonById = (id) => {
-  return axios.get(${API_BASE_URL}/api/pokemons/${id});
+  return axios.get(`${API_BASE_URL}/api/pokemons/${id}`);
 };
 
 // Example function for getting users, if needed
 export const getUsers = () => {
-  return axios.get(${API_BASE_URL}/api/users);
+  return axios.get(`${API_BASE_URL}/api/users`);
 };
